d92d8f395bd59cc65b500837ff221d16feedf043
Miguel Martin <miguel@miguel-martin.com>
2015-08-05 21:06:40 +0930

    Actually make run_samples do something

--- run_samples.sh
+++ run_samples.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-# todo
+interpreter="${1:-./ys420i}"
+samples_dir="${2:-samples}"
+
+[ ! -f $interpreter ] && echo "could not find $interpreter"
+
+for file in $(ls $samples_dir)
+do
+    $interpreter $samples_dir/$file
+done
